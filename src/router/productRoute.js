c2981926e5a22b8942888d79aaf87f2ba58154d3
MD Solaiman <150431927+mdsulimanvassa@users.noreply.github.com>
2024-09-12 14:58:58 +0600

    Update productRoute.js

--- src/router/productRoute.js
+++ src/router/productRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { productreade, singleProduct, deleteProduct, updateProduct, productCreate, updateSingleProduct } = require('../controller/productController');
+const { productreade, singleProduct, deleteProduct, updateProduct, productCreate, updateSingleProduct, productPagination } = require('../controller/productController');
 const { verifyToken, verifyAdmin } = require('../middleware/middleware');
 
 const productRouter = express.Router();
 
 productRouter.get('/products', productreade);
+productRouter.get('/api/products', productPagination);
 productRouter.get('/products/:key', singleProduct);
 productRouter.delete('/product/:id', verifyToken, verifyAdmin, deleteProduct);
 productRouter.get('/product/:id', verifyToken, verifyAdmin, updateProduct);
